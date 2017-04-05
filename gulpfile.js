f76c8ee4c8fe0a6e997c9f374e36d16c50f1fb39
Maxim Siebert <maxim.siebert@gmail.com>
2017-04-05 16:08:33 -0400

    feat: all of the things

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     cssnext = require('postcss-cssnext'),
     rename = require("gulp-rename"),
+    simpleGrid = require('postcss-simple-grid'),
+    easyImport = require('postcss-easy-import'),
     connect = require('gulp-connect');
 
 //CSS
 gulp.task('css', function () {
     var processors = [
+        easyImport,
+        simpleGrid({separator: ['--']}),
         cssnext({browsers: ['last 1 version']})
     ];
     return gulp.src('./src/_*.css')
@@ -21,13 +25,14 @@ gulp.task('css', function () {
 gulp.task('connect', function() {
 	connect.server({
 		root: './',
+        port: 4000,
 		livereload: true
 	});
 });
 
 //Watch
 gulp.task('watch', function() {
-	gulp.watch(['./src/_*.css'], ['css']);
+	gulp.watch(['./src/**/_*.css'], ['css']);
 });
 
 gulp.task('default', ['css', 'connect', 'watch'], function() {});
